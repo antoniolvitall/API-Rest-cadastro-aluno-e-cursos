b253f3b6b0ef7b56986fa2079f5c40f53cda12b6
Antonio Lindo <antoniolvitall@gmail.com>
2023-01-10 19:52:50 -0300

    instalando django rest framework e markdown

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+def alunos(request):
+    if request.method == 'GET':
+        aluno = {'id':1, 'nome':'Antonio'}
+        return JsonResponse(aluno)
 
 # Create your views here.
